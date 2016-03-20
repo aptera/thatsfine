5297bcf3187c2f6c7eaee4b9e4bd4d0f7d8d1435
AJ Karnitis <ajkarnitis@apterainc.com>
2016-03-20 14:34:58 -0400

    Add basic self-hosted Aurelia app
    Creating the shell around the Aurelia app, using a modified verison of the es2016 Aurelia Skeleton application, and having the app be served through browserSync since we aren't sure if everyone will have VS 2015 which we'd want if we were hosting through ASP.NET Core, and we decided we didn't want to host through 4.5.  Obviously that can all change, but it shouldn't have an impact on the development of the actual Aurelia app.

---
+++ build/paths.js
@@ -0,0 +1,12 @@
+var appRoot = 'src/';
+var outputRoot = 'dist/';
+var exportSrvRoot = 'export/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  css: appRoot + '**/*.css',
+  style: 'styles/**/*.css',
+  output: outputRoot
+};
