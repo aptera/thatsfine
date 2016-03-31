ea6f51fdc7cc41f0ec1b7527b582790c6a04ee74
Jared Isaak <jisaak@apterainc.com>
2016-03-31 19:54:25 -0400

    Adding sign in and sign out functionality.

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -5,18 +5,43 @@ var browserSync = require('browser-sync');
 // to create a dev server instance
 // at http://localhost:9000
 gulp.task('serve', ['build'], function(done) {
-  browserSync({
+    var server = browserSync.create("first server"); // Create a named instance
+    var proxy = browserSync.create("proxy");
+
+  server.init({
     online: false,
     open: false,
     port: 9000,
+    https: false,
+    ui: {
+        port: 3001
+    },
     server: {
       baseDir: ['.'],
       middleware: function(req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
       }
+
     }
-  }, done);
+  });
+
+   proxy.init({
+    online: false,
+    open: false,
+    port: 9001,
+      https: true,
+    ui: {
+        port: 3002
+    },
+    proxy: {
+        target: "https://thereisno.trystructure.com/api/v1/",
+        middleware: function (req, res, next) {
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+            next();
+        }
+   }
+  });
 });
 
 // this task utilizes the browsersync plugin
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'create', name: 'create',      moduleId: 'create',      nav: true, title: 'Create' }
-    ]);
+    configureRouter(config, router) {
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['', 'signin'], name: 'signin', moduleId: 'signin', nav: true, title: 'Sign In' },
+            { route: 'create', name: 'create', moduleId: 'create', nav: true, title: 'Create' }
+        ]);
 
-    this.router = router;
-  }
+        this.router = router;
+    }
 }
