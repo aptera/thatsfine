21825764d0b3bf63769bc96dd31d635693900207
David Bohan <madvlad00@googlemail.com>
2016-04-15 10:57:15 -0400

    Add get billing phase type

--- src/create.js
+++ src/create.js
@@ -1,20 +1,23 @@
-import {inject} from 'aurelia-framework';
+import {BindingEngine, inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 
-@inject(HttpClient)
+@inject(HttpClient, BindingEngine)
 export class Create {
     heading = 'Add A New Billable Time Entry';
     clients = [];
-    selectedClient = null;
+    selectedClientName = null;
     projects = [];
     filteredProjects = [];
-    selectedProject = null;
+    selectedProjectId = null;
     phases = [];
     selectedPhase = null;
     billingTypes = [];
     selectedBillingType = null;
+    bindingEngine = null;
+    subscription = null;
 
-    constructor(http) {
+    constructor(http, bindingEngine) {
+      this.bindingEngine = bindingEngine;
       this.clients = [];
         http.configure(config => {
             config
@@ -30,28 +33,30 @@ export class Create {
         });
 
         this.http = http;
+        this.subscription = this.bindingEngine.propertyObserver(this, 'selectedProjectId').subscribe(this.getBillingAndPhaseType);
     }
 
-    getProjectsForClients() {
-
+    getBillingAndPhaseType(projectId, oldValue, model, test, test2) {
+      var billingTypesUrl = 'projects/' + projectId + '/billingTypes';
+      var phaseTypeUrl = 'projects/' + projectId + '/phases';
+      this.billingTypes = [];
+      this.phases = [];
+      this.http.fetch(billingTypesUrl)
+            .then(response => response.json())
+            .then(billingTypes => this.billingTypes = billingTypes)
+            .then(this.http.fetch(phaseTypeUrl)
+                   .then(response => response.json())
+                   .then(phases => this.phases = phases)
+            );
     }
 
     activate() {
         return this.http.fetch('clients?onlyActive=true')
-            .then(
-              response => response.json()
-            )
-            .then(
-              clients => this.clients = clients
-            )
-            .then(
-              this.http.fetch('projects')
-                  .then(
-                    response => response.json()
-                  )
-                  .then(
-                    projects => this.projects = projects
-                  )
+            .then(response => response.json())
+            .then(clients => this.clients = clients)
+            .then(this.http.fetch('projects')
+                  .then(response => response.json())
+                  .then(projects => this.projects = projects)
             );
     }
 }
