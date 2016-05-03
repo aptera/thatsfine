e6a1a94df988aa8f2d8852a4916f657f45031b87
Eric Potter <eric.b.potter@gmail.com>
2016-05-03 11:18:50 -0400

    Move the greeting to its own view

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'hello'], name: 'hello',      moduleId: 'hello',      nav: true, title: 'Hello' }
+    ]);
+
+        this.router = router;
+    
+  }
+  
 }
